1cc3cae071c4a0b9ac996197804fbc56f13e73c4
Ronni Persson <ronni@Artems-iPhone.bonanza.local>
2018-08-07 14:14:27 +0200

    Make tests for Query shallow but add mounted test with Provider as it's a hard dependency

--- src/__tests__/Query.test.js
+++ src/__tests__/Query.test.js
@@ -1,8 +1,10 @@
 /* @flow */
 import React from 'react';
+import { Provider } from 'react-redux';
 import { readEndpoint } from 'redux-json-api';
 import { mount, shallow } from 'enzyme';
 import Query from '../Query';
+import { mockStore } from './utils';
 
 let mockReadEndpoint;
 let props;
@@ -25,8 +27,12 @@ it('renders without throwing', () => {
   expect(() => shallow(<Query {...props} />)).not.toThrow();
 });
 
+it('mounts without throwing with react-redux Provider supplied', () => {
+  expect(() => mount(<Provider store={mockStore({})}><Query {...props} /></Provider>)).not.toThrow();
+});
+
 it('calls readEndpoint with given path', () => {
-  mount(<Query {...props} />);
+  shallow(<Query {...props} />);
   expect(props.dispatch).toHaveBeenCalledWith(readEndpoint(props.endpoint));
 });
 
@@ -36,7 +42,7 @@ it('saves ids of returned resources to state', () => {
       { type: 'users', id: '1', attributes: { name: 'Wonderwoman' } },
     ],
   });
-  const wrapper = mount(<Query {...props} />);
+  const wrapper = shallow(<Query {...props} />);
   setTimeout(() => {
     expect(wrapper.state.resourceIds).toEqual([{ type: 'users', id: '1' }]);
   }, 1);
